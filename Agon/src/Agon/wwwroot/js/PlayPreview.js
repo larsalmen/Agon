b29568e90c6a41feab0d0ad7ff781c898b5745c6
Martin <marny>
2016-11-29 12:25:58 +0100

    Fixade uppspelningsfunktion vid StartQuiz

---
+++ Agon/src/Agon/wwwroot/js/PlayPreview.js
@@ -0,0 +1,53 @@
+﻿var playingCssClass = 'playing',
+    audioObject = null;
+
+$(document).ready(function () {
+    $("#playListener").click(function (e) {
+        var target = e.target;
+        //////
+        var playIcon = 'glyphicon-play';
+        var pauseIcon = 'glyphicon-pause';
+        var target = e.target;
+        if (target !== null && target.classList.contains('play-button')) {
+            if (target.classList.contains(playingCssClass)) {
+                audioObject.pause();
+                //PAUSE ==> PLAY
+                target.children[0].classList.remove(pauseIcon);
+                target.children[0].classList.add(playIcon);
+            } else {
+                if (audioObject) {
+                    audioObject.pause();
+                }
+                //fetchTracks(target.getAttribute('preview-url'), function (data) {
+                audioObject = new Audio(target.getAttribute('preview-url'));
+                audioObject.play();
+                target.classList.add(playingCssClass);
+                //PLAY ==PAUSE
+                target.children[0].classList.remove(playIcon);
+                target.children[0].classList.add(pauseIcon);
+
+                //target.children[0].classList.add(pauseIcon);
+                audioObject.addEventListener('ended', function () {
+                    target.classList.remove(playingCssClass);
+                    //PAUSE ==> PLAY
+                    target.children[0].classList.remove(pauseIcon);
+                    target.children[0].classList.add(playIcon);
+                    //target.children[0].classList.add('glyphicon-play');
+                });
+                audioObject.addEventListener('pause', function () {
+                    target.classList.remove(playingCssClass);
+                    //PAUSE ==> PLAY
+                    target.children[0].classList.remove(pauseIcon);
+                    target.children[0].classList.add(playIcon);
+                });
+                //});
+            }
+        }
+
+
+
+
+
+        /////
+    });
+});
