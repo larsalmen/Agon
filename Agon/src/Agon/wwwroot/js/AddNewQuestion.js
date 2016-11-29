fee1cc5d737f90fb81388f3ac8868fccdcc72c31
Nils Carlsson <nils.j.carlsson@hotmail.com>
2016-11-29 13:30:20 +0100

    Fixed removed song javascript

--- Agon/src/Agon/wwwroot/js/AddNewQuestion.js
+++ Agon/src/Agon/wwwroot/js/AddNewQuestion.js
@@ -3,7 +3,7 @@
     var id = parseInt(document.getElementById('addField').getAttribute('name'));
     var string = '<br /><div id="input-group-' + parseInt(id) + '" class="input-group" style="margin-left:auto;margin-right:auto;min-width:200px;max-width:400px;">';
     string = string + '<span class="input-group-addon">';
-    string = string + '<input type="checkbox" aria-label="..." id="enableDisable' + parseInt(id) + '">';
+    string = string + '<input type="checkbox" aria-label="..."name="item.Checkbox" id="enableDisable' + parseInt(id) + '">';
     string = string + '</span>';
     string = string + '<input type="text" class="form-control" name="item.Text" id="Question-' + parseInt(id) + '">';
     string = string + '<input type="text" class="form-control" name="item.CorrectAnswer" id="Answer-' + parseInt(id) + '">';
@@ -13,10 +13,20 @@
     string = string + '</div>';
     var newInput = string;
     $('div#input-group-' + (id - 1)).after(newInput);
-    id = parseInt(1+id);
+    id = parseInt(1 + id);
     document.getElementById('addField').setAttribute('name', id);
 
 },
     false);
 
 
+$(document).ready(function () {
+    $("#formDiv").click(function (e) {
+        var target = e.target;
+        if (target !== null && target.classList.contains('removeRow')) {
+            $('#input-group-' + target.id).remove()
+
+        }
+
+    })
+});
