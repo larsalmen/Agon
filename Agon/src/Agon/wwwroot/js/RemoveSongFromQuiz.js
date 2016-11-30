301d63401ec16495a768b53138a999c9f8362130
Nils Carlsson <nils.j.carlsson@hotmail.com>
2016-11-30 12:34:21 +0100

    Fixed removal of songs in View and DB

---
+++ Agon/src/Agon/wwwroot/js/RemoveSongFromQuiz.js
@@ -0,0 +1,26 @@
+﻿
+$(document).ready(function () {
+
+    $(".RemoveSong").click(function (e) {
+        e.preventDefault();
+        var target = e.target;
+        $.ajax({
+            url: '/Quiz/RemoveSongFromQuiz',
+            data: {
+                index: target.name
+            },
+            success: function (response) {
+                $("#" + target.name).remove();
+
+                var elements = document.getElementsByClassName("RemoveSong");
+                for (var i = 0; i < elements.length; i++) {
+                    elements[i].setAttribute("name", i);
+                }
+                var elementsDiv = document.getElementsByClassName("RemoveDiv");
+                for (var j = 0; j < elementsDiv.length; j++) {
+                    elementsDiv[j].setAttribute("id", j);
+                }
+            }
+        })
+    })
+})
