a38069a3b797fb8bafd1ccc8c245bda665c03af9
Administrator <Administrator@ACADEMY-6211VM7>
2016-12-01 10:49:50 +0100

    All buttons are now the same size as the largest button.

---
+++ Agon/src/Agon/wwwroot/js/SetButtonSize.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    var trackButtons = document.getElementsByClassName("btn-default");
+    var widestWidth = 0;
+    for (var i = 0; i < trackButtons.length; i++) {
+        var currentWidth = $(trackButtons[i]).width();
+        if (currentWidth > widestWidth) {
+            widestWidth = currentWidth;
+        }
+        //alert("Den här: " + currentWidth + "Den största: " + widestWidth);
+    }
+    //sätt alla knappar till den störstas storlek
+    for (var i = 0; i < trackButtons.length; i++) {
+        trackButtons[i].style.width = (widestWidth*1.05)+"px";
+    }
+});
+
+
