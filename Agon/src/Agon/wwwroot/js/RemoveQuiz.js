67a7eda3efcf4d3e2b30e69b302956cc52f3c0f9
larsalmen <lars@sannagatan.se>
2016-12-13 20:53:20 +0100

    Made the buttons line up on UserLoggedIn page.

--- Agon/src/Agon/wwwroot/js/RemoveQuiz.js
+++ Agon/src/Agon/wwwroot/js/RemoveQuiz.js
@@ -10,16 +10,7 @@
             },
             success: function (response) {
                 $("#" + target.name).remove();
-
-                //var elements = document.getElementsByClassName("RemoveQuiz");
-                //for (var i = 0; i < elements.length; i++) {
-                //    elements[i].setAttribute("name", i);
-
-                //var elementsDiv = document.getElementsByClassName("RemoveDivQuiz");
-                //for (var j = 0; j < elementsDiv.length; j++) {
-                //    elementsDiv[j].setAttribute("id", j);
             }
-        }
-        )
-    })
-})
+        });
+    });
+});
